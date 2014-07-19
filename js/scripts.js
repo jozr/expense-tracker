470dd8e15efa0e539240733aa54637542f163a2d
Andrew Robert Dean Akers & Josephine R Wright <student@epicodus.com>
2014-07-18 17:12:33 -0700

    scripts change

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,30 @@
-
-var expenseTracker = {
-  item: [],
-  cost: [],
-  quantity: [],
+var Expense = {
   total: function() {
     return (this.cost*this.quantity);
   },
-  finalTotal: function() {
-    return (this.cost*this.quantity+this.total());
-    console.log(expenseTracker.finalTotal());
-  }
+  // finalTotal: function() {
+  //   return (this.cost*this.quantity+this.total());
+  //   console.log(expenseTracker.finalTotal());
+  // }
 };
 
 $(document).ready(function() {
   $("form#the-form").submit(function(event) {
     event.preventDefault();
 
-    expenseTracker.item = $("#input-item").val();
-    expenseTracker.cost = parseFloat($("#input-cost").val());
-    expenseTracker.quantity = parseInt($("#input-quantity").val());
+    var newExpense = Object.create(Expense)
+
+    newExpense.item = $("#input-item").val();
+    newExpense.cost = parseFloat($("#input-cost").val());
+    newExpense.quantity = parseInt($("#input-quantity").val());
 
     $("#input-item").val('');
     $("#input-cost").val('');
     $("#input-quantity").val('');
 
-    $("#item").append("<tr>" +  "<td>" + expenseTracker.item + "</td>" + "</tr>")
-    $("#quantity").append("<tr>" + "<td>" + expenseTracker.quantity + "</td>" + "</tr>")
-    $("#total").append("<tr>" +  "<td> $" + expenseTracker.total() + "</td>" + "</tr>")
+    $("#item").append("<tr>" +  "<td>" + newExpense.item + "</td>" + "</tr>")
+    $("#quantity").append("<tr>" + "<td>" + newExpense.quantity + "</td>" + "</tr>")
+    $("#total").append("<tr>" +  "<td> $" + newExpense.total() + "</td>" + "</tr>")
 
   });
 });
